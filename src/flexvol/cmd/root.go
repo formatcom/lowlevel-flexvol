ecd304bd2ad75a2410fb3455bdc4b0de9ee36c21
Vinicio Valbuena <formatcomvinicio@gmail.com>
2019-08-05 10:00:33 -0500

    add system log

--- src/flexvol/cmd/root.go
+++ src/flexvol/cmd/root.go
@@ -2,13 +2,17 @@ package cmd
 
 import (
 	"fmt"
+	"errors"
 	"os"
 	"path/filepath"
 	"github.com/spf13/cobra"
+	"github.com/sirupsen/logrus"
 )
 
 var (
 	APP string
+	//The verbose flag value
+	verbose string
 )
 
 var rootCmd = &cobra.Command{
@@ -17,6 +21,32 @@ var rootCmd = &cobra.Command{
 		// invoke help
 		cmd.HelpFunc()(cmd, []string{})
 	},
+	PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+		level, err := logrus.ParseLevel(verbose)
+		if err != nil {
+			return errors.New(fmt.Sprintf(
+				"not a valid Level: %v\n", verbose))
+		}
+		logrus.SetLevel(level)
+
+		file, err := os.OpenFile(
+				fmt.Sprintf("/var/log/%v.log", APP),
+				os.O_CREATE|os.O_WRONLY, 0666)
+		if err != nil {
+			logrus.Info("Failed to log to file, using default stderr")
+		}else {
+			logrus.SetOutput(file)
+		}
+
+		return nil
+	},
+
+}
+
+func init() {
+	rootCmd.PersistentFlags().StringVarP(&verbose, "verbosity", "v",
+		logrus.WarnLevel.String(),
+		"Log level (debug, info, warn, error, fatal, panic")
 }
 
 func Execute(version string) {
